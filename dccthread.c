1a51f0af510bf6220fde9b0ed530a56d3e27099c
Thiago Padua <paduathiago@outlook.com>
2023-05-08 16:43:41 -0300

    defining first functions

--- dccthread.c
+++ dccthread.c
@@ -4,3 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ucontext.h>
+
+void dccthread_init(void (*func)(int), int param)
+{
+    
+
+}
+
+dccthread_t * dccthread_create(const char *name, void (*func)(int), int param)
+{
+
+}
