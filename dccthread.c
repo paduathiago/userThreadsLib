1b66474242db193056c37ef38e929d6b25d05123
Thiago Padua <paduathiago@outlook.com>
2023-05-08 20:25:11 -0300

    new thread's stack

--- dccthread.c
+++ dccthread.c
@@ -2,10 +2,13 @@
 #include "dlist.h"
 
 #include <malloc.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
+
+#define SIGSTKSZ 8192
 
 struct dlist *ready_threads_list;
 
@@ -17,11 +20,22 @@ typedef struct dccthread
 	ucontext_t context;
 }dccthread_t;
 
+void new_thread_stack(dccthread_t *thread)
+{
+    thread->context.uc_stack.ss_sp = malloc(SIGSTKSZ);
+    thread->context.uc_stack.ss_size = SIGSTKSZ;
+    thread->context.uc_stack.ss_flags = 0;
+}
+
 void dccthread_init(void (*func)(int), int param)
 {
     ready_threads_list = dlist_create();
 
     dccthread_create("main", func, param); // create main thread
+
+    manager_thread = (dccthread_t *) malloc(sizeof(dccthread_t));
+    manager_thread -> name = "manager"; // size of manager
+    getcontext(&manager_thread->context);
 
 }
 
@@ -32,14 +46,12 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int), int param)
     strcpy(new_thread->name, name);
     getcontext(&new_thread->context);
 
-    manager_thread = (dccthread_t *) malloc(sizeof(dccthread_t));
-    manager_thread -> name = (char *) malloc(sizeof(char) * strlen('manager'));
-    manager_thread -> name = "manager";
-    getcontext(&manager_thread->context);
-
     // initialize new thread's stack
+    new_thread_stack(new_thread);
+
 
     // makecontext
+    makecontext(&new_thread->context, func, 1, param);
 
     dlist_push_right(ready_threads_list, new_thread);
 
