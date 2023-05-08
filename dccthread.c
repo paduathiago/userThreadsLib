3edeca2142096154f8e824fbe66190e66a47ef61
Thiago Padua <paduathiago@outlook.com>
2023-05-08 17:34:52 -0300

    adding new thread - first steps

--- dccthread.c
+++ dccthread.c
@@ -1,10 +1,13 @@
 #include "dccthread.h"
 #include "dlist.h"
 
+#include<malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
+
+struct dlist *ready_list;
 
 typedef struct dccthread{
     char *name;
@@ -13,11 +16,33 @@ typedef struct dccthread{
 
 void dccthread_init(void (*func)(int), int param)
 {
-    
+    ready_list = dlist_create();
+
 
 }
 
 dccthread_t * dccthread_create(const char *name, void (*func)(int), int param)
 {
+    dccthread_t *new_thread = (dccthread_t *) malloc(sizeof(dccthread_t));
+    new_thread->name = (char *) malloc(sizeof(char) * strlen(name));
+    strcpy(new_thread->name, name);
+    getcontext(&new_thread->context);
+    
 
+    dlist_push_right(ready_list, new_thread);
+}
+
+void dccthread_yield(void)
+{
+
+}
+
+dccthread_t * dccthread_self(void)
+{
+    
+}
+
+const char * dccthread_name(dccthread_t *tid)
+{
+    return tid->name;
 }
