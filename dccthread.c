c25180b2a0a17cbf5565def669635a66ef41315a
Thiago Padua <paduathiago@outlook.com>
2023-05-08 19:05:54 -0300

    manager thread

--- dccthread.c
+++ dccthread.c
@@ -1,23 +1,27 @@
 #include "dccthread.h"
 #include "dlist.h"
 
-#include<malloc.h>
+#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
 
-struct dlist *ready_list;
+struct dlist *ready_threads_list;
 
-typedef struct dccthread{
+dccthread_t *manager_thread;
+
+typedef struct dccthread
+{
     char *name;
 	ucontext_t context;
 }dccthread_t;
 
 void dccthread_init(void (*func)(int), int param)
 {
-    ready_list = dlist_create();
+    ready_threads_list = dlist_create();
 
+    dccthread_create("main", func, param); // create main thread
 
 }
 
@@ -27,9 +31,19 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int), int param)
     new_thread->name = (char *) malloc(sizeof(char) * strlen(name));
     strcpy(new_thread->name, name);
     getcontext(&new_thread->context);
-    
 
-    dlist_push_right(ready_list, new_thread);
+    manager_thread = (dccthread_t *) malloc(sizeof(dccthread_t));
+    manager_thread -> name = (char *) malloc(sizeof(char) * strlen('manager'));
+    manager_thread -> name = "manager";
+    getcontext(&manager_thread->context);
+
+    // initialize new thread's stack
+
+    // makecontext
+
+    dlist_push_right(ready_threads_list, new_thread);
+
+    return new_thread;
 }
 
 void dccthread_yield(void)
